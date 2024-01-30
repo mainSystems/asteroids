e7b6513bc48d60536d4eb1f5c03d9892ba677b28
dnWork <denial1@yandex.ru>
2024-01-30 16:46:04 +0300

    move data to CommonData

--- src/main/java/main/systems/asteroids/CommonData.java
+++ src/main/java/main/systems/asteroids/CommonData.java
@@ -6,18 +6,17 @@ import java.util.Random;
 
 public class CommonData {
     private static final double SCREEN_X = 1280, SCREEN_Y = 1024;
-    private static final double DELTA_TIME = 1 / 60.0;
-    private static final double ELAPSED_TIME = 0.8;
+    private static final List<String> BACKGROUND_LIST = Arrays.asList("img/background/bg1.jpg", "img/background/bg2.jpg", "img/background/bg3.jpg");
+    private static final List<String> ASTEROID_LIST = Arrays.asList("img/asteroids/ast1.png", "img/asteroids/ast2.png", "img/asteroids/ast3.png");
+    private static final List<String> SHIP_LIST = Arrays.asList("img/ship/ship1.png", "img/ship/ship2.png", "img/ship/ship3.png");
     private static final String TITLE = "Asteroids";
     private static final String ENGINE_LIST = "img/ship/engine/fire1.jpg";
-    private static final List<String> BACKGROUND_LIST = Arrays.asList("img/background/bg1.jpg", "img/background/bg2.jpg", "img/background/bg3.jpg");
-    private static final int asteroidsCount = 16;
+    private static final double DELTA_TIME = 1 / 60.0,  ELAPSED_TIME = 0.8;
     private static final int ASTEROID_SPEED = 30;
-    private static final List<String> ASTEROID_LIST = Arrays.asList("img/asteroids/ast1.png", "img/asteroids/ast2.png", "img/asteroids/ast3.png");
+    private static final int asteroidsCount = 16;
     private static final int asteroidsCountMin = 3;
     private static final String LASER = "img/laser.png";
     private static final int LASER_SPEED = 1600;
-    private static final List<String> SHIP_LIST = Arrays.asList("img/ship/ship1.png", "img/ship/ship2.png", "img/ship/ship3.png");
     private static final int SPACE_SHIP_SPEED = 400;
     private static final int SPACE_SHIP_ROTATION = 3;
     public static double getSCREEN_X() {
